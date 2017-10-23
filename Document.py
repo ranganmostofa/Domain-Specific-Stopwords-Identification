6c9a22aae326439f7620ac36d5c5d65dfc5998ed
Rangan <rm48@rice.edu>
2017-10-23 03:42:11 -0500

    initialized classes

---
+++ Document.py
@@ -0,0 +1,26 @@
+class Document:
+    """
+
+    """
+    def __init__(self, text=""):
+        """
+
+        :param text:
+        """
+        self.text = text
+
+    def get_text(self):
+        """
+
+        :return:
+        """
+        return self.text
+
+    def set_text(self, text):
+        """
+
+        :param text:
+        :return:
+        """
+        self.text = text
+
